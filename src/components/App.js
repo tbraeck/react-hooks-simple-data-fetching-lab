8b77381ab20044f4e731b9770819881f46ac60bf
Tate <tatebraeckel@gmail.com>
2023-01-14 08:21:18 -0700

    commit tree 9c2f870ddeef0258603e1b9aaddd8d48aff0c745

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+
+function App() {
+    const [dogImage, setDogImage] = useState(null)
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(r => r.json())
+            .then((data) => {
+
+                setDogImage(data.message)
+
+            })
+    }, [])
+    if (!dogImage) return <p>Loading...</p>
+    return <img src={dogImage} alt="A Random Dog" />
+}
+
+
+
 // create your App component here
+export default App;
